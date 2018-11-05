041f91107ff0761bf0c0f62cc911a4afa64edf98
Magomedali <web-ali@yandex.ru>
2018-11-05 17:37:02 +0300

    migrations

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -5,6 +5,7 @@ use yii\db\Migration;
 
 class m130524_201442_init extends Migration
 {
+
     public function safeUp()
     {
         $tableOptions = null;
@@ -13,24 +14,110 @@ class m130524_201442_init extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
+
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
-            'username' => $this->string()->null(),
+            'guid' => $this->string(32)->notNull()->unique(),
+            'name' => $this->string(128)->notNull(),
+            'brigade_guid' => $this->string(32)->null(),
+            'technic_guid' => $this->string(32)->null(),
+            'ktu' => $this->float()->notNull()->defaultValue(0),
+            'is_master' => $this->boolean()->notNull()->defaultValue(0),
+
+            'login' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
-            'email' => $this->string()->notNull()->unique(),
-
-            'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
-            'name' => $this->string(255)->notNull(),
-            'phone' => $this->string(10)->notNull(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10)
         ], $tableOptions);
+
+
+        
+
+        $this->createTable('{{%brigade}}', [
+            'id' => $this->primaryKey(),
+            'guid' => $this->string(32)->notNull()->unique(),
+            'name' => $this->string(128)->notNull(),
+
+            'version_id'=>$this->integer()->null(),
+            'isDeleted'=>$this->smallInteger()->null()->defaultValue(0)
+        ], $tableOptions);
+
+
+        $this->createTable('{{%brigade_history}}', [
+            'id' => $this->primaryKey(),
+            'entity_id' => $this->integer()->notNull(),
+
+            'guid' => $this->string(32)->notNull(),
+            'name' => $this->string(128)->notNull(),
+
+            'created_at'=>$this->timestamp(),
+            'type_action'=> $this->integer()->notNull(),
+            'version'=> $this->integer()->notNull(),
+            'creator_id'=> $this->integer()->null(),
+            'isDeleted'=> $this->smallInteger()->null()->defaultValue(0)
+        ], $tableOptions);
+
+        $this->addForeignKey('fk-brigade_history-entity_id',"{{%brigade_history}}",'entity_id',"{{%brigade}}",'id','CASCADE','CASCADE');
+        $this->addForeignKey('fk-brigade_history-creator_id',"{{%brigade_history}}",'creator_id',"{{%user}}",'id','CASCADE','CASCADE');
+        $this->addForeignKey('fk-brigade-version_id',"{{%brigade}}",'version_id',"{{%brigade_history}}",'id','CASCADE','CASCADE');
+        $this->addForeignKey('fk-user-brigade_guid',"{{%user}}",'brigade_guid',"{{%brigade}}",'guid','CASCADE','CASCADE');
+
+        $this->createTable('{{%technic}}', [
+            'id' => $this->primaryKey(),
+            'guid' => $this->string(32)->notNull()->unique(),
+            'name' => $this->string(128)->notNull(),
+            'marka' => $this->string(128)->null(),
+            'number' => $this->string(128)->null(),
+
+            'version_id'=>$this->integer()->null(),
+            'isDeleted'=>$this->smallInteger()->null()->defaultValue(0)
+        ], $tableOptions);
+
+
+        $this->createTable('{{%technic_history}}', [
+            'id' => $this->primaryKey(),
+            'entity_id' => $this->integer()->notNull(),
+
+            'guid' => $this->string(32)->notNull(),
+            'name' => $this->string(128)->notNull(),
+            'marka' => $this->string(128)->null(),
+            'number' => $this->string(128)->null(),
+
+            'created_at'=>$this->timestamp(),
+            'type_action'=> $this->integer()->notNull(),
+            'version'=> $this->integer()->notNull(),
+            'creator_id'=> $this->integer()->null(),
+            'isDeleted'=> $this->smallInteger()->null()->defaultValue(0)
+        ], $tableOptions);
+
+        
+
+        $this->addForeignKey('fk-technic_history-entity_id',"{{%technic_history}}",'entity_id',"{{%technic}}",'id','CASCADE','CASCADE');
+        $this->addForeignKey('fk-technic_history-creator_id',"{{%technic_history}}",'creator_id',"{{%user}}",'id','CASCADE','CASCADE');
+        
+        $this->addForeignKey('fk-technic-version_id',"{{%technic}}",'version_id',"{{%technic_history}}",'id','CASCADE','CASCADE');
+        $this->addForeignKey('fk-user-technic_guid',"{{%user}}",'technic_guid',"{{%technic}}",'guid','CASCADE','CASCADE');
     }
+
 
     public function safeDown()
     {
+        $this->dropForeignKey('fk-user-brigade_guid',"{{%user}}");
+        $this->dropForeignKey('fk-user-technic_guid',"{{%user}}");
+
+        $this->dropForeignKey('fk-brigade-version_id',"{{%brigade}}");
+        $this->dropForeignKey('fk-brigade_history-creator_id',"{{%brigade_history}}");
+        $this->dropForeignKey('fk-brigade_history-entity_id',"{{%brigade_history}}");
+
+        $this->dropForeignKey('fk-technic-version_id',"{{%technic}}");
+        $this->dropForeignKey('fk-technic_history-creator_id',"{{%technic_history}}");
+        $this->dropForeignKey('fk-technic_history-entity_id',"{{%technic_history}}");
+
+        $this->dropTable('{{%brigade_history}}');
+        $this->dropTable('{{%brigade}}');
+        $this->dropTable('{{%technic_history}}');
+        $this->dropTable('{{%technic}}');
         $this->dropTable('{{%user}}');
     }
 }
